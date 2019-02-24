f9573724d180ef108e667a215a0d14f98ee60bad
DrBlack1 <46424019+DrBlack1@users.noreply.github.com>
2019-02-24 12:26:30 +0000

    domchal

---
+++ DOMFiles/DOM/DOMChal/app.js
@@ -0,0 +1,10 @@
+onkeydown = function keyCode() {
+    var key = event.keyCode;
+    document.getElementById('key').innerHTML = key;
+    var code = event.code;
+    document.getElementById('letter').innerHTML = `${code}`;
+    var keywhich = event.which;
+    document.getElementById('keywhich').innerHTML = `${keywhich}`;
+    var letter = event.key;
+    document.getElementById('code').innerHTML = `${letter}`;
+}
