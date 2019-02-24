3b53aae8280172787bac9d4d6e56bb08881d03c2
DrBlack1 <46424019+DrBlack1@users.noreply.github.com>
2019-02-24 12:17:18 +0000

    Delete app.js

--- DOMFiles/DOM/6 - Changing Element Attributes/app.js
+++
@@ -1,7 +0,0 @@
-const image = document.getElementById('#image');
-const input = document.getElementById('#input');
-const button = document.getElementById('#submit');
-
-button.addEventListener('click', () => {
-    image.src = input.value;
-})
