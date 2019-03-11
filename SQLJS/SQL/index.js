551863b9fdd78b62b9ba0f97f68f5032bb481cda
DrBlack1 <drblack1no@gmail.com>
2019-03-11 07:32:38 +0000

    update text

--- SQLJS/SQL/index.js
+++
@@ -1,8 +0,0 @@
-const Sql = require('./queries');
-
-const sql = new Sql(); 
-
-sql.connect();
-sql.select("Name, date_of_birth, gender", "employee_detail");
-sql.update("Name", "employee_detail", "Warren")
-sql.end();
--- SQLJS/index.js
+++
@@ -1,6 +0,0 @@
-const Sql = require('./queries');
-const sql = new Sql();
-
-sql.connect();
-sql.jobTitleMaxSalary('current_job_detail');
-sql.disconnect();
