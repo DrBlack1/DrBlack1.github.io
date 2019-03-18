7550548045d070087e645ef73df71895a4b0892f
yourUserName <patrick.mack1@yahoo.com>
2019-03-18 19:51:32 +0000

    made changes to mongodb

--- mongoDB/models/user.js
+++ mongoDB/models/user.js
@@ -8,3 +8,21 @@ const user = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', user);
+
+// const mongoose = require('mongoose');
+
+// const user = new mongoose.Schema({
+//     firstName: {type:String, required: true},
+//     lastName: {type:String, required: true},
+//     age: {type:Number, required: true},
+//     location: {type:String, required: true},
+
+// const query  = user.findOne({
+//         'firstName' : ''
+// });
+
+//  user.find({},(err,users) => {
+//      console.log(users)
+//  });
+ 
+// module.exports = mongoose.model('User', user);
