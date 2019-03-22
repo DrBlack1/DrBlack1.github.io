c49110f157d9afa6f24e7f7fff7f783301352c50
yourUserName <patrick.mack1@yahoo.com>
2019-03-22 19:16:46 +0000

    updated tdl

---
+++ React2/To-do-List/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import ToDoList from './components/ToDoList'
+
+
+ReactDOM.render(
+    <div>
+        <ToDoList/>
+    </div>,
+    document.getElementById("container")
+);
+
