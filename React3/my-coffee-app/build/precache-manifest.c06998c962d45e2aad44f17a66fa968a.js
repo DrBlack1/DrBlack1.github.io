e8560fc4a36410492105f48d3f8b64161ee47a9f
yourUserName <patrick.mack1@yahoo.com>
2019-05-11 19:53:48 +0100

    Added coffee

---
+++ React3/my-coffee-app/build/precache-manifest.c06998c962d45e2aad44f17a66fa968a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c54fe7aa27aeff1ea76d",
+    "url": "/my-coffee-app/static/js/runtime~main.c54fe7aa.js"
+  },
+  {
+    "revision": "4b83f230658931fa534c",
+    "url": "/my-coffee-app/static/js/main.4b83f230.chunk.js"
+  },
+  {
+    "revision": "2d38de127de4238b196c",
+    "url": "/my-coffee-app/static/js/1.2d38de12.chunk.js"
+  },
+  {
+    "revision": "4b83f230658931fa534c",
+    "url": "/my-coffee-app/static/css/main.148de8c8.chunk.css"
+  },
+  {
+    "revision": "2d38de127de4238b196c",
+    "url": "/my-coffee-app/static/css/1.d9d86538.chunk.css"
+  },
+  {
+    "revision": "79d6e0baa18e20f501635f0c09bb9b3b",
+    "url": "/my-coffee-app/index.html"
+  }
+];
