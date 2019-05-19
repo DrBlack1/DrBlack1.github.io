dc7157a533f698282bc7677cc0cdcffcc4a076c2
yourUserName <patrick.mack1@yahoo.com>
2019-05-19 11:08:21 +0100

    Added pics

---
+++ CarmeetSite/index.js
@@ -0,0 +1,19 @@
+document.getElementById('location').addEventListener("click", function(){
+    let page2 = document.getElementById('page2')
+    page2.scrollIntoView({block:'end', behavior:'smooth'});
+})
+
+document.getElementById('aboutus').addEventListener("click", function(){
+    let page3 = document.getElementById('page3')
+    page3.scrollIntoView({block:'end', behavior:'smooth'});
+})
+
+document.getElementById('contact').addEventListener("click", function(){
+    let page4 = document.getElementById('page4')
+    page4.scrollIntoView({block:'end', behavior:'smooth'});
+})
+
+document.getElementById('showcase').addEventListener("click", function(){
+    let page5 = document.getElementById('page5')
+    page5.scrollIntoView({block:'end', behavior:'smooth'});
+})
